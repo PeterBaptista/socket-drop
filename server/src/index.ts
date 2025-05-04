da8e4880abf897842e63881d238aeb8c24369f5d
Pedro Baptista <pedrolk2012@gmail.com>
2025-05-04 18:11:40 -0300

    Add server setup with WebSocket and React utility functions

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,18 +1,24 @@
 import { env } from "@/common/utils/envConfig";
 import { app, logger } from "@/server";
+import { setupWebSocketServer } from "@/lib/ws";
+import http from "node:http";
 
-const server = app.listen(env.PORT, () => {
-  const { NODE_ENV, HOST, PORT } = env;
-  logger.info(`Server (${NODE_ENV}) running on port http://${HOST}:${PORT}`);
+const server = http.createServer(app);
+
+setupWebSocketServer(server);
+
+server.listen(env.PORT, () => {
+    const { NODE_ENV, PORT, HOST } = env;
+    logger.info(`Server (${NODE_ENV}) running at http://${HOST}:${PORT}`);
 });
 
 const onCloseSignal = () => {
-  logger.info("sigint received, shutting down");
-  server.close(() => {
-    logger.info("server closed");
-    process.exit();
-  });
-  setTimeout(() => process.exit(1), 10000).unref(); // Force shutdown after 10s
+    logger.info("sigint received, shutting down");
+    server.close(() => {
+        logger.info("server closed");
+        process.exit();
+    });
+    setTimeout(() => process.exit(1), 10000).unref(); // Force shutdown after 10s
 };
 
 process.on("SIGINT", onCloseSignal);
